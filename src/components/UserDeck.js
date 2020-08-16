83df83e0c4d9e7efdfada925544910f43d7b49dc
akashrajum7 <akashrajum7@gmail.com>
2020-08-16 14:31:50 +0530

    Add loading animation

--- src/components/UserDeck.js
+++ src/components/UserDeck.js
@@ -4,11 +4,13 @@ import Container from "react-bootstrap/Container";
 import UserCard from "./UserCard";
 import parse from "parse-link-header";
 import Pagination from "react-js-pagination";
+import Spinner from "react-bootstrap/Spinner";
 
 function Users() {
   const [Page, setPage] = useState(1);
   const [lastPage, setLastPage] = useState(984);
   const [Forks, setForks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   let userCards = Forks.map((fork) => {
     let { login, avatar_url, id } = fork.owner;
@@ -17,6 +19,7 @@ function Users() {
 
   useEffect(() => {
     const getForks = async () => {
+      setIsLoading(true);
       const response = await octokit.request(
         "GET /repos/{owner}/{repo}/forks?page={Page}",
         {
@@ -30,28 +33,39 @@ function Users() {
         setLastPage(parsed.last.page);
       }
       setForks(response.data);
+      setIsLoading(false);
     };
     getForks();
   }, [Page, lastPage]);
 
-  return (
-    <Container className="mt-4">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-center">
-        {userCards}
-      </div>
-      <Pagination
-        activePage={Page}
-        itemsCountPerPage={30}
-        totalItemsCount={30 * lastPage}
-        pageRangeDisplayed={5}
-        itemClass="page-item"
-        linkClass="page-link"
-        onChange={(pageNumber) => {
-          setPage(pageNumber);
-        }}
-      />
-    </Container>
-  );
+  if (!isLoading) {
+    return (
+      <Container className="mt-4">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-center">
+          {userCards}
+        </div>
+        <Pagination
+          activePage={Page}
+          itemsCountPerPage={30}
+          totalItemsCount={30 * lastPage}
+          pageRangeDisplayed={5}
+          itemClass="page-item"
+          linkClass="page-link"
+          onChange={(pageNumber) => {
+            setPage(pageNumber);
+          }}
+        />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <div className="row justify-content-center loading-cont">
+          <Spinner animation="grow" />
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default Users;
