d77d063284ce5b591bbc8355535127cad619dcec
akashrajum7 <akashrajum7@gmail.com>
2020-08-16 09:20:18 +0530

    Remove console log

--- src/config/octokit.js
+++ src/config/octokit.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.GITHUB_ACCESS_TOKEN);
-
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({ auth: `${process.env.GITHUB_ACCESS_TOKEN}` });
 
